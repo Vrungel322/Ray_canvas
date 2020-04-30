f66848bf5e997b3a58f074ce31571b4f23acead5
NikitaRomanenko <romanenko.nikita@pdffiller.team>
2020-04-30 13:53:18 +0300

    init

---
+++ android/app/src/main/kotlin/com/ray/app/flutter_ray_canvas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ray.app.flutter_ray_canvas
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
