4a33cefd27a4688601fbb30bd4e4b0a4f6055f37
NikitaRomanenko <romanenko.nikita@pdffiller.team>
2020-10-18 00:40:58 +0300

    Serpinskiy triangle

--- lib/canvas_widget.dart
+++ lib/canvas_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ray_canvas/data.dart';
 import 'package:vector_math/vector_math.dart' as math;
@@ -11,14 +13,33 @@ class CanvasWidget extends StatefulWidget {
 }
 
 class _CanvasWidgetState extends State<CanvasWidget> {
-  double userX = 0;
-  double userY = 0;
-  DrawData drawData = DrawData.instance; // need to be crated only once
+  var dots = [math.Vector2(0, 0)];
+  var generator = DotGenerator(
+      [
+        math.Vector2(0, 0),
+        math.Vector2(0, 1080),
+        math.Vector2(720, 1080),
+      ],
+      math.Vector2(720 / 4, 1080 / 4));
+
 
   @override
   Widget build(BuildContext context) {
-    double areaWidth = MediaQuery.of(context).size.width;
-    double areaHeight = MediaQuery.of(context).size.height;
+    double areaWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double areaHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+
+    Future.value(generator.generate()).then((list){
+      setState(() {
+        dots = list;
+      });
+    });
+
 
     return Scaffold(
       appBar: AppBar(
@@ -26,9 +47,7 @@ class _CanvasWidgetState extends State<CanvasWidget> {
           IconButton(
             icon: Icon(Icons.refresh),
             onPressed: () {
-              setState(() {
-                drawData.generateWalls(areaWidth, areaHeight / 2);
-              });
+              setState(() {});
             },
           )
         ],
@@ -36,101 +55,40 @@ class _CanvasWidgetState extends State<CanvasWidget> {
       ),
       body: Center(
           child: Listener(
-        onPointerMove: (PointerEvent details) {
-          setState(() {
-            userX = details.localPosition.dx;
-            if (details.localPosition.dy > 0 && details.localPosition.dy < areaHeight / 2)
-              userY = details.localPosition.dy;
-            else
-              userY = areaHeight / 2;
-            drawData.particle = Particle(math.Vector2(userX, userY));
-            if (drawData.walls.isEmpty) drawData.generateWalls(areaWidth, areaHeight / 2);
-          });
-        },
-        child: CustomPaint(
-          size: Size(areaWidth, areaHeight),
-          painter: RayPainter(userX, userY, drawData, areaWidth, areaHeight),
-        ),
-      )),
+            onPointerMove: (PointerEvent details) {
+              // setState(() {});
+            },
+            child: CustomPaint(
+              size: Size(areaWidth, areaHeight),
+              painter: RayPainter(dots),
+            ),
+          )),
     );
   }
 }
 
 class RayPainter extends CustomPainter {
-  double userX;
-  double userY;
-  double areaWidth;
-  double areaHeight;
-  DrawData drawData;
+  List<math.Vector2> dots;
+
   final Paint wallPaint = Paint()
     ..strokeWidth = 2
     ..color = Colors.white;
-  final Paint rayPaint = Paint()
-    ..strokeWidth = 1
-    ..color = Color.fromRGBO(255, 255, 255, 20);
-  final Paint pointerPaint = Paint()
-    ..strokeWidth = 4
-    ..style = PaintingStyle.stroke
-    ..color = Colors.redAccent;
 
-  RayPainter(this.userX, this.userY, this.drawData, this.areaWidth, this.areaHeight);
+  RayPainter(this.dots);
 
   @override
   void paint(Canvas canvas, Size size) {
     //background
     canvas.drawColor(Colors.black, BlendMode.clear);
+    var offsets = dots.map((d) {
+      return Offset(d.x, d.y);
+    }).toList();
 
-    //draw all walls
-    drawData.walls.forEach((b) {
-      canvas.drawLine(Offset(b.a.x, b.a.y), Offset(b.b.x, b.b.y), wallPaint);
+    dots.forEach((d) {
+      canvas.drawPoints(PointMode.points, offsets, wallPaint);
     });
-
-    if (drawData.particle != null) {
-      //rays 2D---------------------------------------------------------------------------------------------------------
-      drawData.particle.intersect(drawData.walls).forEach((rAndI) {
-        Offset startPoint = Offset(rAndI.ray.pos.x, rAndI.ray.pos.y);
-        Offset endPoint;
-        if (rAndI.intersectPoint == null) {
-          endPoint = Offset(rAndI.ray.dir.x, rAndI.ray.dir.y);
-//          endPoint = startPoint;
-        } else {
-          endPoint = Offset(rAndI.intersectPoint.x, rAndI.intersectPoint.y);
-        }
-        canvas.drawLine(startPoint, endPoint, rayPaint);
-      });
-
-      canvas.drawCircle(Offset(userX, userY), 20, pointerPaint);
-
-      //walls 3D--------------------------------------------------------------------------------------------------------
-      List<double> scene = drawData.particle.intersect(drawData.walls).map((rAndI) {
-        return rAndI.intersectPoint?.distanceTo(math.Vector2(userX, userY)) ?? -1;
-      }).toList();
-
-      double sceneH = areaHeight / 2;
-      double sceneW = areaWidth;
-      for (int i = 0; i < scene.length; i++) {
-        double h = map(scene[i], 0, sceneW, sceneH, 0);
-        if (scene[i] == -1) h = 0; // no intersection with wall - need to set 0
-
-        Rect r = Rect.fromCenter(
-            center: Offset(sceneW / scene.length * i, sceneH * 1.5),
-            width: sceneW / scene.length,
-            height: h);
-
-        int col = 255 - map(scene[i].toInt(), 0, sceneW, 0, 255).toInt();
-        canvas.drawRect(
-            r,
-            Paint()
-              ..strokeWidth = 4
-              ..style = PaintingStyle.fill
-              ..color = Color.fromARGB(255, col, col, col));
-      }
-    }
   }
 
-  double map(n, start1, stop1, start2, stop2) {
-    return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-  }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
--- lib/data.dart
+++ lib/data.dart
@@ -2,135 +2,26 @@ import 'dart:math';
 
 import 'package:vector_math/vector_math.dart';
 
-const int WALLS_COUNT = 4;
-const double RAY_ANGLE_DELTA = 0.1;
 
-class DrawData {
-  DrawData._internal();
+class DotGenerator {
+  List<Vector2> _vertexrs;
+  Vector2 _point;
+  List<Vector2> dots = [];
 
-  static final DrawData instance = DrawData._internal();
+  final _random = new Random();
 
-  List<Wall> walls = [];
-  Particle particle;
+  DotGenerator(this._vertexrs, this._point){
+    dots = [_point];
+  }
 
-  void generateWalls(double areaWidth, double areaHeight) {
-    walls.clear();
-    for (int i = 0; i < WALLS_COUNT; i++) {
-      Vector2 random1 = Vector2.random();
-      Vector2 random2 = Vector2.random();
-      walls.add(Wall(Vector2(areaWidth * random1.x, areaWidth * random1.y),
-          Vector2(areaWidth * random2.x, areaWidth * random2.y)));
+  List<Vector2> generate() {
+    for (int i = 0; i < 100; i++) {
+      var lastElem = dots.last;
+      var randomVertex = _vertexrs[_random.nextInt(_vertexrs.length)];
+      var delta = (lastElem - randomVertex) / 2;
+      _point = lastElem - delta;
+      dots.add(_point);
     }
-  }
-}
-
-class Particle {
-  Vector2 pos;
-  List<Ray> rays = [];
-
-  Particle(this.pos) {
-    for (double i = 0; i < 40; i += RAY_ANGLE_DELTA) {
-      rays.add(Ray(Vector2(pos.x, pos.y), i));
-    }
-  }
-
-  List<RayAndIntersectPoint> intersect(List<Wall> walls) {
-    var rayAndIntersectPoint = rays.map((r) {
-      return RayAndIntersectPoint(r, r.intersectionPointWithNearestWall(walls));
-    }).toList();
-    return rayAndIntersectPoint;
-  }
-}
-
-class Wall {
-  final Vector2 a;
-  final Vector2 b;
-
-  Wall(this.a, this.b);
-}
-
-class Ray {
-  Vector2 pos;
-  Vector2 dir;
-  double angle;
-
-  Ray(this.pos, this.angle) {
-    num rad = angle * pi / 180;
-    dir = Vector2(cos(rad) * 10000, sin(rad) * 10000); // 10000 just for make ray larger
-  }
-
-  Vector2 intersect(Wall wall) {
-    double x1 = pos.x;
-    double y1 = pos.y;
-    double x2 = dir.x;
-    double y2 = dir.y;
-
-    double x3 = wall.a.x;
-    double y3 = wall.a.y;
-    double x4 = wall.b.x;
-    double y4 = wall.b.y;
-
-    // it can be 0 if ray and wall are parallel || or has no intersection
-    double den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-    if (den != 0) {
-      double t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
-      double u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
-
-      final hasIntersectionWithWall = t > 0 && t < 1 && u > 0 && u < 1;
-      if (hasIntersectionWithWall) {
-        double x = x1 + t * (x2 - x1);
-        double y = y1 + t * (y2 - y1);
-        return Vector2(x, y);
-      } else
-        null;
-    } else
-      return null;
-  }
-
-  /// returns distance between ray star position and [intersectPoint]
-  double distanceToRayStartFrom(Vector2 intersectPoint) {
-    double length;
-    if (intersectPoint == null) {
-      length = null;
-    } else {
-      length = sqrt(pow(intersectPoint.x - pos.x, 2) + pow(intersectPoint.y - pos.y, 2));
-    }
-    return length;
-  }
-
-  /// return intersectPoint with nearest wall that ray will intersect
-  Vector2 intersectionPointWithNearestWall(List<Wall> walls) {
-    var list = walls.map((w) {
-      Vector2 intersectPoint = intersect(w); // intersect point with wall
-      double d = distanceToRayStartFrom(intersectPoint); // distance between ray start and intersect point
-      return IntersectPointAndDistanceFromRayStart(intersectPoint, d);
-    }).toList();
-    list.sort(); // after sort - first element will be intersect point with nearest wall
-    return list.first.intersectPoint;
-  }
-}
-
-// Help objects --------------------------------------------------------------------------------------------------------
-class RayAndIntersectPoint {
-  final Ray ray;
-  final Vector2 intersectPoint;
-
-  RayAndIntersectPoint(this.ray, this.intersectPoint);
-}
-
-class IntersectPointAndDistanceFromRayStart extends Comparable<IntersectPointAndDistanceFromRayStart> {
-  final Vector2 intersectPoint;
-  final double d;
-
-  IntersectPointAndDistanceFromRayStart(this.intersectPoint, this.d);
-
-  @override
-  int compareTo(other) {
-    if (d == null) return 100;
-    if (other.d == null) return -100;
-    if (d < other.d)
-      return -1;
-    else
-      return 1;
+    return dots;
   }
 }
